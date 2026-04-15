6f1a4c312b710411e998d7049a267c4a60624194
handinu <110212479+handinu@users.noreply.github.com>
2026-04-15 17:29:55 -0400

    wednesday 1

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ let cursorImg;
 
 //Pointing finger cursor
 function preload(){
-  cursorImg = loadImage("pointerfinger.png");
+  cursorImg = loadImage("pointerfingerS1.png");
   //Upload a new image with as a png so there is no white background
   //resource https://editor.p5js.org/xinxin/sketches/6GBSGiQGT
 }
